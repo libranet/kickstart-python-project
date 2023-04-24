26fe381ad331d9a75de30e8301c8a9daa941b177
Wouter Vanden Hove <wouter@libranet.eu>
2023-04-24 17:15:04 +0200

    typo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,7 +8,7 @@ with_fastapi = int("{{ cookiecutter.with_fastapi }}")
 with_flask = int("{{ cookiecutter.with_flask }}")
 with_typer = int("{{ cookiecutter.with_typer }}")
 support_rtd = int("{{ cookiecutter.support_rtd }}")
-git_platform= int("{{ cookiecutter.git_platform }}")
+git_platform= "{{ cookiecutter.git_platform }}"
 
 if not with_fastapi:
     os.remove(f"src/{package_name}/api.py")
