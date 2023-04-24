77df4ca984b86818557f44c2da68656aea11772d
Wouter Vanden Hove <wouter@libranet.eu>
2023-04-24 17:07:57 +0200

    update

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Read Cookiecutter configuration.
+package_name = "{{ cookiecutter.__package_name_snake_case }}"
+with_django = int("{{ cookiecutter.with_django }}")
+with_fastapi = int("{{ cookiecutter.with_fastapi }}")
+with_flask = int("{{ cookiecutter.with_flask }}")
+with_typer = int("{{ cookiecutter.with_typer }}")
+suppport_rtd = int("{{ cookiecutter.suppport_rtd }}")
+git_platform= int("{{ cookiecutter.git_platform }}")
+
+if not with_fastapi:
+    os.remove(f"src/{package_name}/api.py")
+    os.remove("tests/test_api.py")
+
+if not with_typer:
+    os.remove(f"src/{package_name}/cli.py")
+    os.remove("tests/test_cli.py")
+
+if not suppport_rtd:
+    os.remove(".readthedocs.yaml")
+
+
+if git_platform != "gitHub":
+    shutil.rmtree(".github/")
+elif git_platform != "gitLab":
+    os.remove(".gitlab-ci.yml")
